ba5313016a045a8ba40f475b3b788c55d08df82e
Benjamin <benjaminonuzurike963@gmail.com>
2024-06-02 01:07:29 -0600

    implemented database

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+class Tag(models.Model):
+    caption = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.caption
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField() # check documentation for more validation options
+    # bio = models.TextField()
+    
+    def full_name(self):
+        'return the full name of the author'
+        return f'{self.first_name} {self.last_name}'
+    def __str__(self):
+        'return the string representation of the author object'
+        return self.full_name()
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    # image = models.ImageField(upload_to='static/blog/images/') # upload_to is the directory where the image will be saved, upload_to will handle file uploads
+    image_name = models.CharField(max_length=100)
+    excerpt = models.CharField(max_length=200)
+    content = models.TextField(validators=[MinLengthValidator(10)]) 
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=False)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name='posts') # one author can have many posts
+    tag = models.ManyToManyField(Tag, related_name='tags') # one post can have many tags
+
+
+
+ 
--- blog/views.py
+++ blog/views.py
@@ -1,81 +1,13 @@
-from datetime import date
 from django.shortcuts import render
+from blog.models import Post, Author, Tag
 
 # Create your views here.
 # These are the posts that will be displayed on the blog page, it will be stored in a database in the future.
-all_posts = [
-    {
-        "slug": "hike-in-the-mountains",
-        "image": "mountains.jpg",
-        "author": "Maximilian",
-        "date": date(2021, 7, 21),
-        "title": "Mountain Hiking",
-        "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "programming-is-fun",
-        "image": "coding.jpg",
-        "author": "Maximilian",
-        "date": date(2022, 3, 10),
-        "title": "Programming Is Great!",
-        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "into-the-woods",
-        "image": "woods.jpg",
-        "author": "Maximilian",
-        "date": date(2020, 8, 5),
-        "title": "Nature At Its Best",
-        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    }
-]
-def get_date(post):
-    return post['date']
 
 def starting_page(request):
-    # months = list(challenge_text.keys())
-    # posts = list(challenge_text.values())
-    sorted_posts = sorted(all_posts, key=get_date) # get_date is a callback function that returns the date of each post
-    latest_posts = sorted_posts[-3:]
+    # sorted_posts = sorted(all_posts, key=get_date) # get_date is a callback function that returns the date of each post
+    # latest_posts = sorted_posts[-3:]
+    latest_posts = Post.objects.all().order_by('-date')[:3] # Django converts the query into an SQL code and it takes into account the limit and offset parameters.
     # We want the latest 3 posts to be displayed on the home page -3:-1 gives us the last 3 posts
     context = {
         'posts': latest_posts
@@ -84,6 +16,7 @@ def starting_page(request):
     return render(request, 'blog/index.html', context)
 
 def posts(request):
+    all_posts = Post.objects.all() # retrieve all posts from the database
     context = {
         'all_posts': all_posts
     }
@@ -92,10 +25,7 @@ def posts(request):
 def post_detail(request, slug):
     # next is a built-in function that returns the next item in an iterable if it matches a condition
     # identified_post = next(post for post in all_posts if post['slug'] == slug)
-    for post in all_posts:
-        if post['slug'] == slug:
-            identified_post=post
-            break
+    identified_post = Post.objects.get(slug=slug) # get the post with the specified slug from the database
     context = {
         'post': identified_post
     }
